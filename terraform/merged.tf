a40aecf00b4ca1daff66983bc17d7b54f770641b
Bouali <youssefbouali206@gmail.com>
2025-11-05 09:43:40 +0100

    m

--- terraform/merged.tf
+++ terraform/merged.tf
@@ -758,11 +758,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.60"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 4.60"
     }
   }
 
