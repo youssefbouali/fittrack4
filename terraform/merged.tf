349775d2160c8563ecfb71ede67c2e2a88f6000e
Bouali <youssefbouali206@gmail.com>
2025-11-10 21:56:23 +0100

    Up

--- terraform/merged.tf
+++ terraform/merged.tf
@@ -116,6 +116,19 @@ resource "aws_s3_bucket" "frontend" {
   tags   = var.tags
 }
 
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+
 resource "aws_s3_bucket_versioning" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   versioning_configuration {
