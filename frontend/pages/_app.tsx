d9586d8c9b50baa69e09440f50c3e0a6d93130e4
Bouali <youssefbouali206@gmail.com>
2025-11-10 18:25:11 +0100

    Up

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import { awsConfig, validateAwsConfig } from '../config/aws';
-import { initializeAuth, AuthService } from '../services/authService';
+import { AuthService } from '../services/authService';
 import { Amplify } from 'aws-amplify';
 import '../styles/globals.css';
 
@@ -12,32 +12,27 @@ export default function App({ Component, pageProps }: AppProps) {
     // Validate and initialize AWS config
     validateAwsConfig();
 
-    // Configure Amplify with Cognito
+    // Configure Amplify with Cognito (نسخة حديثة)
     Amplify.configure({
-	  Auth: {
-		Cognito: {
-		  region: awsConfig.region,
-		  userPoolId: awsConfig.userPoolId,
-		  userPoolWebClientId: awsConfig.clientId,
-		  identityPoolId: awsConfig.identityPoolId
-		}
-	  },
-	  Storage: {
-		S3: {
-		  region: awsConfig.region,
-		  bucket: awsConfig.s3Bucket,
-		  identityPoolId: awsConfig.identityPoolId
-		}
-	  }
-	});
-
+      Auth: {
+        region: awsConfig.region,
+        userPoolId: awsConfig.userPoolId,
+        userPoolWebClientId: awsConfig.clientId,
+        identityPoolId: awsConfig.identityPoolId,
+      },
+      Storage: {
+        region: awsConfig.region,
+        bucket: awsConfig.s3Bucket,
+        identityPoolId: awsConfig.identityPoolId,
+      },
+    });
 
     // Check if user is already logged in
     const checkAuth = async () => {
       try {
         const user = await AuthService.getCurrentUser();
         if (user) {
-          // User is logged in, could dispatch to Redux here
+          // User is logged in, يمكنك dispatch للـRedux هنا إذا أردت
         }
       } catch (error) {
         // No user logged in
